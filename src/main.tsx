74441158e8d205c90ecbb55d8e91bbfb1ca3fab5
Reetesh <reetesh.dooleea@nsisolution.com>
2025-07-14 23:01:39 -0400

    feat: trying routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename='/app'>
-      <App />
+      <Routes>
+        <Route path='index.html' element={<Navigate to='/' />} />
+        <Route path='/' element={<App />} />
+        <Route path='*' element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
